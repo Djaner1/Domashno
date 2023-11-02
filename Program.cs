fa58a8a2342dd180e5981fe3595d1dfdc11e0ed9
Djaner1 <124555259+Djaner1@users.noreply.github.com>
2023-11-02 19:31:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10._19._23
+namespace _11._2._23
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter V-");
-            float v = float.Parse(Console.ReadLine());
-            Console.Write("Enter T-");
-            float t = float.Parse(Console.ReadLine());
-            Console.Write("Enter a-");
-            float a=float.Parse(Console.ReadLine());
-            double s=(v*t)+(Math.Pow(a*t,2)/2);
-            Console.WriteLine("S="+s);
-
+            int[,] suma = new int[2, 2];
+            int obshto1=0;
+            int obshto2=0;
+            for(int i = 0; i <2; i++)
+            {
+                for(int o=0; o<2; o++)
+                {
+                    suma[i, o] = int.Parse(Console.ReadLine());
+                     obshto1 = suma[0, 0] + suma[0, 1];
+                     obshto2 = suma[1,0] + suma[1, 1];
+                }
+            }
+            Console.WriteLine("Sumata za 1 red e "+obshto1);
+            Console.WriteLine("Sumata za 2 red e "+obshto2);
 
         }
     }
