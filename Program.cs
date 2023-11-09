502182b5df90b18916b7ec5debfc457caee13178
Djaner1 <124555259+Djaner1@users.noreply.github.com>
2023-11-09 19:17:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _11._2._23
+namespace _11._9._23
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[,] suma = new int[2, 2];
-            int obshto1=0;
-            int obshto2=0;
-            for(int i = 0; i <2; i++)
+           
+            int n = int.Parse(Console.ReadLine());
+            int m=int.Parse(Console.ReadLine());
+            int[,] masiv = new int[n, m];
+            for(int i = 0; i < n; i++)
             {
-                for(int o=0; o<2; o++)
+                for(int j = 0; j < m; j++)
                 {
-                    suma[i, o] = int.Parse(Console.ReadLine());
-                     obshto1 = suma[0, 0] + suma[0, 1];
-                     obshto2 = suma[1,0] + suma[1, 1];
+                    masiv[i,j]=int.Parse(Console.ReadLine());
+
                 }
             }
-            Console.WriteLine("Sumata za 1 red e "+obshto1);
-            Console.WriteLine("Sumata za 2 red e "+obshto2);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if(masiv[i,j]/2==0)
+                    {
+                        Console.WriteLine(masiv[i,j]);
+                    }
+
+                }
+            }
+
+
 
         }
     }
