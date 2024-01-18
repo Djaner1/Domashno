5177bc26de8a66316ffe707de596dd3146ba33d6
Djaner1 <124555259+Djaner1@users.noreply.github.com>
2024-01-18 20:16:43 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Visochina_na_uchenici
+namespace Zad_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             double max = double.MinValue;
-            var height=new Dictionary<string,double>();
-            height["Dimitar"] = 1.80;
-            height["Viktor"] = 1.77;
+double min=double.MaxValue;
+            var height = new Dictionary<string, double>();            
+            height["Dimitar"] = 1.90;
+            height["Viktor"] = 1.89;
             height["Ivan"] = 1.50;
-            height["Aleks"]=1.84;
-            foreach(var i in height)
-            {
-                Console.WriteLine("{0}--{1}",i.Key,i.Value);                
-            }
+            height["Aleks"] = 1.86;
+            height["Djemal"] = 1.68;
+            height["Anil"] = 1.58;
+            height["Vladislav"] = 1.79;
+            height["Petur"] = 1.93;
+            height["Emil"] = 1.65;
             height["Djaner"] = 1.78;
+            height["Dean"] = 2.00;
+
+            Console.WriteLine("Vuv basketbolniq otbor igraqt:");
             foreach (var i in height)
             {
-                if(i.Value>max) max=i.Value;
-
+                if (i.Value >=1.85)
+                {
+                    Console.WriteLine(i.Key);
+                    if(i.Value >max) max = i.Value;
+                }
             }
-            var keyOfMaxValue =
-    height.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
-            Console.WriteLine("Nai visokiq uchenik-"+keyOfMaxValue);
-            var total = height.Sum(v => v.Value);
-            double sr = total / height.Count;
-            Console.WriteLine(total);
-            Console.WriteLine("sredna visochina-"+sr);
-            Console.WriteLine("Broi na uchenici="+height.Count);
+            height["Yoan"] = 1.80;
+            height["Stoyan"] = 1.50;
+            height["Boris"] = 1.69;
+            height["Hristo"] = 1.30;
+            height["Preslav"] = 1.80;
+
+
+            Console.WriteLine("Vuv futbolniq otbor igraqt:");
+            foreach (var i in height)
+            {
+                if (i.Value < 1.85)
+                {
+                    Console.WriteLine(i.Key);
+                    if(i.Value<min) min = i.Value;
+                }
+            }
+
+            Console.WriteLine("Proverqvane za 'Djaner'");
+            if (height.ContainsKey("Djaner"))
+            {
+                Console.WriteLine("Da ima takuv uchenik");
+            }else
+                Console.WriteLine("Nqma takuv uchenik");
+
+            Console.WriteLine("Nai niskiqt ot futbolniq otbor-"+min);
+            Console.WriteLine("Nai visokiqt ot basketbolniq otbor-"+max);
 
 
 
